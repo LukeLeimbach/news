be4f2fe95af7c9aaf7903506fea9a4c88561ddf6
Luke Leimbach <luke.leimbach@gmail.com>
2022-10-13 12:20:58 -0600

    sendgrid DONT PUSH - HIDDEN KEY

--- django_project/settings.py
+++ django_project/settings.py
@@ -136,4 +136,11 @@ LOGOUT_REDIRECT_URL = "home"
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+
+DEFAULT_FROM_EMAIL = "DjangoNewspaper"
+EMAIL_HOST = "smtp.sendgrid.net"
+EMAIL_HOST_USER = "apikey"
+EMAIL_HOST_PASSWORD = "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"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
